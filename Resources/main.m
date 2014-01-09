b8904c5eadd403f6973d1766d33ac7a569a35f65
Shawn Busolits <shawn.busolits@gmail.com>
2014-01-09 07:38:14 -0800

    Committing latest changes.

---
+++ Resources/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  SampleAppV3
+//
+//  Created by Charles Pratt on 10/26/12.
+//
+
+#import <UIKit/UIKit.h>
+#import "IMASampleAppDelegate.h"
+
+int main(int argc, char *argv[]) {
+ @autoreleasepool {
+   return UIApplicationMain(argc,
+                            argv,
+                            nil,
+                            @"IMASampleAppDelegate");
+ }
+}
