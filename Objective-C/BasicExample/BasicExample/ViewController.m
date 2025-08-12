415bc153634992a5e4d4191d8634435a843e607f
Jackson Sui <jsui@google.com>
2025-08-12 07:53:43 -0700

    Adds documentation region tags.
    PiperOrigin-RevId: 794109893

--- Objective-C/BasicExample/BasicExample/ViewController.m
+++ Objective-C/BasicExample/BasicExample/ViewController.m
@@ -1,8 +1,13 @@
+// [START player_imports]
 #import "ViewController.h"
 
 @import AVFoundation;
+// [END player_imports]
+// [START ima_import]
 @import GoogleInteractiveMediaAds;
+// [END ima_import]
 
+// [START player_variables]
 @interface ViewController () <IMAAdsLoaderDelegate, IMAAdsManagerDelegate>
 
 /// Content video player.
@@ -13,7 +18,9 @@
 
 /// UIView in which we will render our AVPlayer for content.
 @property(nonatomic, weak) IBOutlet UIView *videoView;
+// [END player_variables]
 
+// [START ima_variables]
 // SDK
 /// Entry point for the SDK. Used to make ad requests.
 @property(nonatomic, strong) IMAAdsLoader *adsLoader;
@@ -23,9 +30,11 @@
 
 /// Main point of interaction with the SDK. Created by the SDK as the result of an ad request.
 @property(nonatomic, strong) IMAAdsManager *adsManager;
+// [END ima_variables]
 
 @end
 
+// [START player_setup]
 @implementation ViewController
 
 // The content URL to play.
@@ -47,11 +56,6 @@ NSString *const kTestAppAdTagUrl = @"https://pubads.g.doubleclick.net/gampad/ads
   [self setUpContentPlayer];
 }
 
-- (IBAction)onPlayButtonTouch:(id)sender {
-  [self requestAds];
-  self.playButton.hidden = YES;
-}
-
 #pragma mark Content Player Setup
 
 - (void)setUpContentPlayer {
@@ -66,16 +70,34 @@ NSString *const kTestAppAdTagUrl = @"https://pubads.g.doubleclick.net/gampad/ads
   playerLayer.frame = self.videoView.layer.bounds;
   [self.videoView.layer addSublayer:playerLayer];
 
+  // [START set_content_playhead]
   // Set up our content playhead and contentComplete callback.
   self.contentPlayhead = [[IMAAVPlayerContentPlayhead alloc] initWithAVPlayer:self.contentPlayer];
   [[NSNotificationCenter defaultCenter] addObserver:self
                                            selector:@selector(contentDidFinishPlaying:)
                                                name:AVPlayerItemDidPlayToEndTimeNotification
                                              object:self.contentPlayer.currentItem];
+  // [END set_content_playhead]
 }
+
+- (IBAction)onPlayButtonTouch:(id)sender {
+  [self requestAds];
+  self.playButton.hidden = YES;
+}
+// [END player_setup]
+
+// [START content_complete]
+- (void)contentDidFinishPlaying:(NSNotification *)notification {
+  // Make sure we don't call contentComplete as a result of an ad completing.
+  if (notification.object == self.contentPlayer.currentItem) {
+    [self.adsLoader contentComplete];
+  }
+}
+// [END content_complete]
 
 #pragma mark SDK Setup
 
+// [START request_ads]
 - (void)setupAdsLoader {
   self.adsLoader = [[IMAAdsLoader alloc] initWithSettings:nil];
   self.adsLoader.delegate = self;
@@ -94,16 +116,11 @@ NSString *const kTestAppAdTagUrl = @"https://pubads.g.doubleclick.net/gampad/ads
                                                        userContext:nil];
   [self.adsLoader requestAdsWithRequest:request];
 }
-
-- (void)contentDidFinishPlaying:(NSNotification *)notification {
-  // Make sure we don't call contentComplete as a result of an ad completing.
-  if (notification.object == self.contentPlayer.currentItem) {
-    [self.adsLoader contentComplete];
-  }
-}
+// [END request_ads]
 
 #pragma mark AdsLoader Delegates
 
+// [START ads_loader_delegate]
 - (void)adsLoader:(IMAAdsLoader *)loader adsLoadedWithData:(IMAAdsLoadedData *)adsLoadedData {
   // Grab the instance of the IMAAdsManager and set ourselves as the delegate.
   self.adsManager = adsLoadedData.adsManager;
@@ -120,23 +137,29 @@ NSString *const kTestAppAdTagUrl = @"https://pubads.g.doubleclick.net/gampad/ads
   NSLog(@"Error loading ads: %@", adErrorData.adError.message);
   [self.contentPlayer play];
 }
+// [END ads_loader_delegate]
 
 #pragma mark AdsManager Delegates
 
+// [START ads_manager_delegate]
 - (void)adsManager:(IMAAdsManager *)adsManager didReceiveAdEvent:(IMAAdEvent *)event {
   // When the SDK notified us that ads have been loaded, play them.
   if (event.type == kIMAAdEvent_LOADED) {
     [adsManager start];
   }
 }
+// [END ads_manager_delegate]
 
+// [START ads_manager_error]
 - (void)adsManager:(IMAAdsManager *)adsManager didReceiveAdError:(IMAAdError *)error {
   // Something went wrong with the ads manager after ads were loaded. Log the error and play the
   // content.
   NSLog(@"AdsManager error: %@", error.message);
   [self.contentPlayer play];
 }
+// [END ads_manager_error]
 
+// [START pause_resume_requests]
 - (void)adsManagerDidRequestContentPause:(IMAAdsManager *)adsManager {
   // The SDK is going to play ads, so pause the content.
   [self.contentPlayer pause];
@@ -146,5 +169,6 @@ NSString *const kTestAppAdTagUrl = @"https://pubads.g.doubleclick.net/gampad/ads
   // The SDK is done playing ads (at least for now), so resume the content.
   [self.contentPlayer play];
 }
+// [END pause_resume_requests]
 
 @end
