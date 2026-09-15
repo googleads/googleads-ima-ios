8185730bce9c8a8857fb6cb4c061474246cdfc6b
Jackson Sui <jsui@google.com>
2026-09-15 10:22:50 -0700

    Adopt Scene Delegate pattern for client-side Objective-C sample apps.
    PiperOrigin-RevId: 981897356

---
+++ Objective-C/SampleVideoPlayer/SampleVideoPlayer/SVPSceneDelegate.h
@@ -0,0 +1,12 @@
+@import UIKit;
+
+/**
+ * Scene delegate for SampleVideoPlayer (SVP).
+ *
+ * Manages the application's UIWindow and root view controller lifecycle.
+ */
+@interface SVPSceneDelegate : UIResponder <UIWindowSceneDelegate>
+
+@property(nonatomic, strong) UIWindow *window;
+
+@end
---
+++ Objective-C/SampleVideoPlayer/SampleVideoPlayer/SVPSceneDelegate.m
@@ -0,0 +1,23 @@
+#import "SVPSceneDelegate.h"
+
+@implementation SVPSceneDelegate
+
+- (void)scene:(UIScene *)scene
+    willConnectToSession:(UISceneSession *)session
+                 options:(UISceneConnectionOptions *)connectionOptions {
+  if (![scene isKindOfClass:[UIWindowScene class]]) {
+    return;
+  }
+  if (!self.window) {
+    UIWindowScene *windowScene = (UIWindowScene *)scene;
+    self.window = [[UIWindow alloc] initWithWindowScene:windowScene];
+    NSString *storyboardName =
+        ([UIDevice currentDevice].userInterfaceIdiom == UIUserInterfaceIdiomPad) ? @"iPad"
+                                                                                 : @"iPhone";
+    UIStoryboard *storyboard = [UIStoryboard storyboardWithName:storyboardName bundle:nil];
+    self.window.rootViewController = [storyboard instantiateInitialViewController];
+    [self.window makeKeyAndVisible];
+  }
+}
+
+@end
